feb8afe9d9959fcd07faf06a0338074117d25c89
Tae <taeyoung92kim@gmail.com>
2018-11-01 21:34:00 -0400

    Basic Website Setup

---
+++ logic.js
@@ -0,0 +1,25 @@
+var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+});
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+});
+
+//make a function to create array for database or somehting
+//initialize a variable fwith an empty array of object?
+//var myArray = [];
+// for (var i = 0; i < 3; i++) {
+//     var myObj = {};
+//     for (var x = 0; x < 1; x++) {
+//         myObj["URL" + x] = "val" + x;
+//     }
+//     myArray.push(myObj);
+// }
+// console.log(myArray)
+
+$(document).ready(function () {
+    $('.modal').modal();
+});
