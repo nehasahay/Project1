5c27dde3f6961bc474d83d5d196c90cfdd2a9914
N <username@users.noreply.github.com>
2018-11-03 12:19:44 -0400

    just some skeleton function comments

--- logic.js
+++ logic.js
@@ -1,3 +1,12 @@
+// // let today = whatever today's date is
+
+// function wikipedia(date) {
+//     // Get a random event that happened on date
+//     let wikipediaArticle = eventThatHappenedOn(date);
+//     let title = title(wikipediaArticle);
+//     // let place = place???(wikipediaArticle);
+// };
+
 function Wikipedia(date = "December_3") {
     // let queryURL = "https://en.wikipedia.org/w/api.php?format=json&action=parse&page=Wikipedia:Selected_anniversaries/" + date;
     // let queryURL = "https://en.wikipedia.org/w/api.php?format=json&action=parse&text={{Wikipedia:Selected_anniversaries/" + date + "}}&contentmodel=wikitext"
@@ -31,7 +40,7 @@ function Wikipedia(date = "December_3") {
             let end = listOfEvents.indexOf("</li>");
             let event = listOfEvents.substring(start, end);
 
-            let dashIndex = event.indexOf("-");
+            let dashIndex = event.indexOf(" – ");
             console.log(dashIndex);
             let pageIndex = event.indexOf("wiki/", dashIndex);
             console.log(pageIndex);
@@ -71,3 +80,26 @@ function wikiImage(wikipediaPage) {
 }
 
 Wikipedia();
+
+
+// function newYorkTimes(date) {
+//     // Get a news article that happened today
+// };
+
+// $(document).on("click", "#submit-button", function () {
+//     let date = $("#input-box-ID").val().trim();
+//     // wikipedia(date)
+//     // newYorkTimes(date)
+// });
+
+// $(document).on("click", ".favorite", function() {
+//     // push the wiki link to an array that goes to firebase
+// });
+
+
+// wikipedia(today);
+// newYorkTimes(today);
+
+// // user validation: don't let them pick a date from the future, or give them 2017
+
+// document.write("stuff you get from parse.text")
