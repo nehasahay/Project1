73cbc2c5f098b3f749cc41b6e45976b290db8ba6
Esaray74 <you@example.com>
2018-11-06 15:21:44 -0500

    local changes

--- logic.js
+++ logic.js
@@ -83,14 +83,14 @@ function Wikipedia(date = "December_3") {
 
 function wikiImage(wikipediaPage) {
     $.ajax({
-         url: "https://en.wikipedia.org/w/api.php",
-         data: {
-             action: "parse",
-             format: "json",
-             page: wikipediaPage,
-             prop: "images",
-             origin: "*"
-         },
+        url: "https://en.wikipedia.org/w/api.php",
+        data: {
+            action: "parse",
+            format: "json",
+            page: wikipediaPage,
+            prop: "images",
+            origin: "*"
+        },
         method: "GET"
     }).then(function (response) {
         console.log(response);
@@ -98,5 +98,36 @@ function wikiImage(wikipediaPage) {
     });
 }
 
-
 Wikipedia();
+
+//-----------------Firebase Auth----------------
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+        document.getElementById("user_div").style.display = "block";
+        document.getElementById("login_div").style.display = "none";
+        var user = firebase.auth().currentUser;
+        if (user != null) {
+            var email_id = user.email;
+            document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+        }
+    } else {
+        // No user is signed in.
+        document.getElementById("user_div").style.display = "none";
+        document.getElementById("login_div").style.display = "block";
+    }
+});
+function login() {
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        window.alert("Error : " + errorMessage);
+    });
+}
+function logout() {
+    firebase.auth().signOut();
+}
